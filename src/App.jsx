dcc781e769f2daf3b99ec3ef15e7ab2143fc5ded
yrnoh <noh1oo1@naver.com>
2024-12-16 21:28:14 +0900

    feat: 헤더 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import './App.css';
+import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+      <Header />
       <Outlet />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AppTailwindCSS';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import ErrorPage from './pages/ErrorPage';
 import CanvasDetail from './pages/CanvasDetail';
 
 const router = createBrowserRouter([
@@ -27,15 +27,16 @@ const router = createBrowserRouter([
         element: <Contact />,
       },
       {
-        path: 'canvases/id',
+        path: 'canvases/:id',
         element: <CanvasDetail />,
       },
     ],
+    errorElement: <ErrorPage />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
