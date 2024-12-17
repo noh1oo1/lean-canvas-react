3bdeed76b09a4b504a2a6082b0a8c0458154036c
yrnoh <yrnoh@rsupport.com>
2024-12-17 14:30:25 +0900

    feat: 로딩, 에러 ui 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,28 @@ import { getCanvases } from '../api/canvas';
 import CanvasList from '../components/CanvasList';
 import SearchBar from '../components/SearchBar';
 import ViewToggle from '../components/ViewToggle';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
 
 function Home() {
   const [searchText, setSearchText] = useState();
   const [isGridView, setIsGridView] = useState(true);
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   async function fetchData(params) {
-    const response = await getCanvases(params);
-    setData(response.data);
+    try {
+      setIsLoading(true);
+      setError(null);
+      await new Promise(resolver => setTimeout(resolver, 2000));
+      const response = await getCanvases(params);
+      setData(response.data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -23,18 +36,27 @@ function Home() {
   };
 
   return (
-    <div className="container mx-auto px-4 py-16">
+    <>
       <div className="mb-6 flex flex-col sm:flex-row items-center justify-between">
         <SearchBar searchText={searchText} setSearchText={setSearchText} />
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
-      <CanvasList
-        filteredData={data}
-        isGridView={isGridView}
-        searchText={searchText}
-        onDeleteItem={handleDeleteItem}
-      />
-    </div>
+      {isLoading && <Loading />}
+      {error && (
+        <Error
+          message={error.message}
+          onRetry={() => fetchData({ title_like: searchText })}
+        />
+      )}
+      {!isLoading && !error && (
+        <CanvasList
+          filteredData={data}
+          isGridView={isGridView}
+          searchText={searchText}
+          onDeleteItem={handleDeleteItem}
+        />
+      )}
+    </>
   );
 }
 
